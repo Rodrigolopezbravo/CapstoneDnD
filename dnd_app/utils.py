77f9feaa5abb76d9046acc559242413d8c547d73
Nicolas Ignacio Moraga Ortiz <107308579+zubza90@users.noreply.github.com>
2025-09-15 22:24:23 -0300

    Delete dnd_app directory

--- dnd_app/utils.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-def hash_password(plain):
-    return generate_password_hash(plain)
-
-def check_password(hashed, plain):
-    return check_password_hash(hashed, plain)
